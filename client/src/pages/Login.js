c3512768e5c20e1d46543d44e63cc2e420635efe
Raviteja2003 <ravitejat0406@gmail.com>
2023-02-17 11:50:43 +0530

    Login UI added

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,30 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import { Form,Input } from 'antd'
 const Login = () => {
+  //form handler
+  const onfinishHandler =(values) =>   { 
+  console.log(values)
+    }
   return (
     <div>
-        <h1>Login Page</h1>
+        <div className='form-container'>
+          <Form layout='vertical' onFinish={onfinishHandler}  className='register-form'>
+              <h3 className='text-center'>Login Form</h3>
+              <Form.Item label="Email" name="email">
+                <Input type="email" required/> 
+              </Form.Item>
+              <Form.Item label="Password" name="password">
+                <Input type="password" required/>
+              </Form.Item>
+              <Link to="/register" className='m-2'>
+                Not a user Register here
+              </Link>
+              <button className='btn btn-primary' type='submit'>
+                Login
+              </button>
+          </Form>
+      </div>
     </div>
   )
 }
