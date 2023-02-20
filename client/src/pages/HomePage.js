832327513bf878fd1b6e7b9f8c440d1d5ab3ea2e
Raviteja2003 <ravitejat0406@gmail.com>
2023-02-20 22:11:21 +0530

    Authorization completed

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,17 @@
-import React from 'react'
-
+import React ,{useEffect} from 'react'
+import axios from 'axios'
 const HomePage = () => {
+  //login user data
+  const getUserData = async() =>{
+    const res = await axios.post("/api/v1/user/getUserData",{},{
+      headers:{
+        Authorization : "Bearer "+localStorage.getItem("token"),
+      },
+    })
+  }
+  useEffect(()=>{
+    getUserData()
+  },[])
   return (
     <div>
         <h1>HomePage</h1>
