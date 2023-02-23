48e8ca3789c26415eb757e0f1fca2be743013391
Raviteja2003 <ravitejat0406@gmail.com>
2023-02-23 21:13:18 +0530

    spinner added

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,21 @@ import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Login from "./pages/Login";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
+import { useSelector } from "react-redux";
+import Spinner from "./components/Spinner";
 function App() {
+  const {loading} = useSelector(state => state.alerts)
   return (
    <>
     <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<HomePage/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
-      </Routes>
+      { loading ? (
+           <Spinner/>):(
+        <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
+        </Routes>)
+    }
     </BrowserRouter>
    </>
   );
